5dfbb2d55470989761f7d9b0d7b5fdfd9ac6edc7
Andrew-Zheng <andzheng@gmail.com>
2022-01-15 13:54:32 -0500

    Added coroutineScope & extracted Retrofit Api service call from MainActivity

--- app/src/main/java/com/example/retrofitpracticepokeapi/MainActivity.kt
+++ app/src/main/java/com/example/retrofitpracticepokeapi/MainActivity.kt
@@ -12,27 +12,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val coroutineScope = CoroutineScope(Dispatchers.IO)
-
-        coroutineScope.launch {
-            val response = PokemonApi.retrofitService.getPokedexList()
-
-            if (response.isSuccessful) {
-                Log.i("Response", "Success!")
-                val listOfPokemon = response.body()
-                val arraySize = listOfPokemon?.results?.size
-                Log.i("ResponseArraySize", "Array Size: $arraySize")
-                Log.i("ResponseListResults", "Pokemon List: ${listOfPokemon?.results}")
-                
-                for (pokemon in listOfPokemon?.results!!) {
-                    Log.i("Response", "Pokemon Object: $pokemon")
-                    Log.i("Response", "Pokemon Name: ${pokemon.name}")
-                    Log.i("Response", "Pokemon Url: ${pokemon.url}")
-                }
-            } else {
-                Log.i("Response", "Failed")
-            }
-        }
     }
 }
--- app/src/main/java/com/example/retrofitpracticepokeapi/answerslist/PokedexViewModel.kt
+++ app/src/main/java/com/example/retrofitpracticepokeapi/answerslist/PokedexViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.retrofitpracticepokeapi.answerslist
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.retrofitpracticepokeapi.network.PokemonApi
 import com.example.retrofitpracticepokeapi.network.PokemonList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class PokedexViewModel : ViewModel() {
 
@@ -11,4 +16,31 @@ class PokedexViewModel : ViewModel() {
     val pokemonList: LiveData<List<PokemonList>>
         get() = _pokemonList
 
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    init {
+        getPokemonList()
+    }
+
+    private fun getPokemonList() {
+        coroutineScope.launch {
+            val response = PokemonApi.retrofitService.getPokedexList()
+
+            if (response.isSuccessful) {
+                Log.i("Response", "Success!")
+                val listOfPokemon = response.body()
+                val arraySize = listOfPokemon?.results?.size
+                Log.i("ResponseArraySize", "Array Size: $arraySize")
+                Log.i("ResponseListResults", "Pokemon List: ${listOfPokemon?.results}")
+
+                for (pokemon in listOfPokemon?.results!!) {
+                    Log.i("Response", "Pokemon Object: $pokemon")
+                    Log.i("Response", "Pokemon Name: ${pokemon.name}")
+                    Log.i("Response", "Pokemon Url: ${pokemon.url}")
+                }
+            } else {
+                Log.i("Response", "Failed")
+            }
+        }
+    }
 }
