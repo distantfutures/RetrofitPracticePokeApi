53a3f2e76fbd67f8f25f858c882a2a6879484bab
Andrew-Zheng <andzheng@gmail.com>
2022-01-15 14:25:40 -0500

    Lazy initialized ViewModel in Fragment, binded viewModel to binding, added data variable to layout.

--- app/src/main/java/com/example/retrofitpracticepokeapi/answerslist/PokedexFragment.kt
+++ app/src/main/java/com/example/retrofitpracticepokeapi/answerslist/PokedexFragment.kt
@@ -6,10 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.retrofitpracticepokeapi.R
 import com.example.retrofitpracticepokeapi.databinding.FragmentPokedexBinding
 
 class PokedexFragment : Fragment() {
+    /**
+     * Lazily initialize our [OverviewViewModel].
+     */
+    private val viewModel: PokedexViewModel by lazy {
+        ViewModelProvider(this).get(PokedexViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +26,12 @@ class PokedexFragment : Fragment() {
         val binding: FragmentPokedexBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_pokedex, container, false
         )
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
 
         return binding.root
     }
--- app/src/main/java/com/example/retrofitpracticepokeapi/answerslist/PokedexViewModel.kt
+++ app/src/main/java/com/example/retrofitpracticepokeapi/answerslist/PokedexViewModel.kt
@@ -4,22 +4,26 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.retrofitpracticepokeapi.network.Pokemon
 import com.example.retrofitpracticepokeapi.network.PokemonApi
 import com.example.retrofitpracticepokeapi.network.PokemonList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class PokedexViewModel : ViewModel() {
 
-    val _pokemonList = MutableLiveData<List<PokemonList>>()
-    val pokemonList: LiveData<List<PokemonList>>
+    val _pokemonList = MutableLiveData<Pokemon>()
+    val pokemonList: LiveData<Pokemon>
         get() = _pokemonList
 
-    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
     init {
         getPokemonList()
+        Log.i("Response", "getPokemonList INITIALIZED")
     }
 
     private fun getPokemonList() {
@@ -37,6 +41,7 @@ class PokedexViewModel : ViewModel() {
                     Log.i("Response", "Pokemon Object: $pokemon")
                     Log.i("Response", "Pokemon Name: ${pokemon.name}")
                     Log.i("Response", "Pokemon Url: ${pokemon.url}")
+                    _pokemonList.value = pokemon
                 }
             } else {
                 Log.i("Response", "Failed")
