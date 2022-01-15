8bc7f576d697ef7d1238fa430bf60f66d4bdc8fd
Andrew-Zheng <andzheng@gmail.com>
2022-01-15 13:51:29 -0500

    Added pokemonList LiveData to ViewModel

--- app/src/main/java/com/example/retrofitpracticepokeapi/answerslist/PokedexViewModel.kt
+++ app/src/main/java/com/example/retrofitpracticepokeapi/answerslist/PokedexViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.retrofitpracticepokeapi.answerslist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.retrofitpracticepokeapi.network.PokemonList
 
 class PokedexViewModel : ViewModel() {
+
+    val _pokemonList = MutableLiveData<List<PokemonList>>()
+    val pokemonList: LiveData<List<PokemonList>>
+        get() = _pokemonList
 
 }
