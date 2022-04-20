1f373431600b1ebb850beec69b9e95e8bbe8718b
Andrew Zheng <andzheng@gmail.com>
2022-04-20 10:48:51 -0400

    Refactored PokemonApiService interface

--- app/src/main/java/com/example/retrofitpracticepokeapi/network/PokemonApiService.kt
+++ app/src/main/java/com/example/retrofitpracticepokeapi/network/PokemonApiService.kt
@@ -8,18 +8,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
-//Use Moshi Builder to create Moshi object w. KotlinJsonAdapterFactory
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
 // Fetches JSON response. Create Retrofit Builder using scalars converter factory & give base url of web service.
 private val retrofit = Retrofit.Builder()
-    //Turns Json response into Kotlin Objects
-    //.addConverterFactory(MoshiConverterFactory.create(moshi))
     .addConverterFactory(GsonConverterFactory.create())
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
@@ -27,10 +21,11 @@ private val retrofit = Retrofit.Builder()
 
 // Implement API service interface that return JSON data as a string (through Scalars)
 interface PokemonApiService {
-    // Gets answers objects
-    @GET("pokemon/?offset=0&limit=151")
-    suspend fun getPokedexList():
-            Response<PokemonObject>
+    @GET("pokemon")
+    suspend fun getPokedexList(
+        @Query("offset") offset: Int = 0,
+        @Query("limit") limit: Int = 20
+    ): Response<PokemonObject>
 
     @GET("pokemon/{id}")
     suspend fun getSpritesList(@Path("id") id: Int):
